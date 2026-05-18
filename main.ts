35ec7094e23114842eea9e9cf95843a521d0a4de
takumi kimoto <2009takumi@gmail.com>
2026-05-18 09:13:07 +0900

    commit

--- main.ts
+++ main.ts
@@ -3,58 +3,66 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-async function failingStar(){
-    for (let i:number = 0; i < 10; i++) {
-    const img = document.createElement("img");
-    console.log(img);
-    const starUrl = new URL("star.png", import.meta.url).href;
-    img.src = starUrl;
-    img.alt = "star";
-    img.style.position = "fixed";
-    img.style.left = "0";
-    img.style.top = "0";
-    img.style.width = `${gsap.utils.random(24, 64)}px`;
-    img.style.pointerEvents = "none";
-    img.style.zIndex = "-1";
+async function failingStar() {
+    for (let i: number = 0; i < 10; i++) {
+        const img = document.createElement("img");
+        console.log(img);
+        const starUrl = new URL("star.png", import.meta.url).href;
+        img.src = starUrl;
+        img.alt = "star";
+        img.style.position = "fixed";
+        img.style.left = "0";
+        img.style.top = "0";
+        img.style.width = `${gsap.utils.random(24, 64)}px`;
+        img.style.pointerEvents = "none";
+        img.style.zIndex = "-1";
 
-    document.body.appendChild(img);
-    let startX:number = -100;
-    const startY = () => gsap.utils.random(-window.innerHeight / 2, window.innerHeight / 2);
-    gsap.fromTo(
-        img,
-        {
-            x: startX,
-            y: startY,
-        },
-        {
-            x: window.innerWidth + 10,
-            y: `+=${window.innerHeight + 200}`,
-            repeat: -1,
-            duration: gsap.utils.random(6, 12),
-            delay: i * 0.5,
-            ease: "none"
-        }
-    );
-    gsap.fromTo(
-        img,
-        {
-            rotate: 0
-        },
-        {
-            rotate: 360,
-            repeat: -1,
-            duration: 3,
-            ease: "none"
-        }
-    );
-}
+        document.body.appendChild(img);
+        let startX: number = -100;
+        const startY = () => gsap.utils.random(-window.innerHeight / 2, window.innerHeight / 2);
+        gsap.fromTo(
+            img,
+            {
+                x: startX,
+                y: startY,
+            },
+            {
+                x: window.innerWidth + 10,
+                y: `+=${window.innerHeight + 200}`,
+                repeat: -1,
+                duration: gsap.utils.random(6, 12),
+                delay: i * 0.5,
+                ease: "none"
+            }
+        );
+        gsap.fromTo(
+            img,
+            {
+                rotate: 0
+            },
+            {
+                rotate: 360,
+                repeat: -1,
+                duration: 3,
+                ease: "none"
+            }
+        );
+    }
 }
 failingStar();
-window.addEventListener("scroll",()=>{
+window.addEventListener("scroll", () => {
     let scroll = window.scrollY;
-    const background:any = document.querySelector(".background");
+    const background: any = document.querySelector(".background");
 
-    if(background != null){
+    if (background != null) {
         background.style.background.position.y = `${scroll / 2}px`;
     }
-})
+});
+
+gsap.to(".light", {
+    rotationY: 180,
+    duration: 8,
+    yoyo: true,
+    repeat: -1,
+    ease: "sine.inOut",
+});
